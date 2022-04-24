eb45f78b8a11007aff8044a7d6c8629bfa9f0bce
Karan Bhasin <bhasink69@gmail.com>
2022-04-24 12:40:07 +0530

    Contact Us form develpment on about us and contact page

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,91 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import AOS from 'aos'
 import Nav from '../components/header/Nav'
 import Footer from '../components/footer/Footer'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import { useToasts } from 'react-toast-notifications'
 
 const About = () => {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [mobileNo, setMobileNo] = useState('')
+  const [query, setQuery] = useState('')
+  const router = useRouter()
+  const { addToast } = useToasts()
+
   useEffect(() => {
     AOS.init({
       duration: 2000,
     })
   }, [])
+
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (name == '') {
+      addToast('Please enter the name!', { appearance: 'error' })
+
+      return false
+    }
+
+    if (email == '') {
+      addToast('Please enter the email!', { appearance: 'error' })
+      return false
+    }
+
+    if (IsEmail(email) == false) {
+      addToast('Incorrect email!', { appearance: 'error' })
+
+      return false
+    }
+
+    if (mobileNo == '') {
+      addToast('Please enter the mobile number!', { appearance: 'error' })
+      return false
+    }
+
+    if (mobileNo.length != 10) {
+      addToast('Mobile number must be of ten digits!', { appearance: 'error' })
+      return false
+    }
+
+    if (query == '') {
+      addToast('Please enter the query!', { appearance: 'error' })
+      return false
+    }
+
+
+    try {
+      const data = await axios.post(`https://phplaravel-709751-2547471.cloudwaysapps.com/api/contact-us`,{
+          "name":name,
+          "email":email,
+          "mobile_no":mobileNo,
+          "query":query,
+      });
+  
+      if(data.status == 200){
+        addToast("Success!", { appearance: 'success' });
+        router.push("/thanks");
+      }
+
+      //
+    } catch (err) {
+      console.log(err)
+      addToast('Invalid! Please try again.', { appearance: 'error' })
+    }
+  }
+
+  const IsEmail = (email) => {
+    let regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
+    if (!regex.test(email)) {
+      return false
+    } else {
+      return true
+    }
+  }
 
   return (
     <>
@@ -194,34 +271,51 @@ const About = () => {
               <br />
               consectetur, adipisci velit...
             </p>
-            <form>
-              <div className="form-row customfrms">
-                <div className="form-group col-md-6">
-                  <input
-                    type="textr"
-                    className="form-control"
-                    placeholder="First Name"
-                  />
+            <form onSubmit={handleSubmit}>
+                <div className="customfrms lssmg">
+                  <div className="form-group">
+                    <input
+                      type="textr"
+                      className="form-control"
+                      placeholder="First Name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Email Id"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Mobile Number"
+                      value={mobileNo}
+                      onChange={(e) =>setMobileNo(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="form-group">
+                                    <textarea
+                                      type="text"
+                                      className="form-control"
+                                      placeholder="Query"
+                                      value={query}
+                                      onChange={(e) => setQuery(e.target.value)}
+                                    ></textarea>
+                                  </div>
+
+                  <div className="form-group text-center">
+                    <button type='submit'>Submit</button>
+                  </div>
                 </div>
-                <div className="form-group col-md-6">
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Email Id"
-                  />
-                </div>
-                <div className="form-group col-md-6">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Phone Number"
-                  />
-                </div>
-                <div className="form-group col-md-6">
-                  <button>Submit</button>
-                </div>
-              </div>
-            </form>
+              </form>
           </div>
         </div>
       </section>
--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,14 +1,91 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import AOS from 'aos'
 import Nav from '../components/header/Nav'
 import Footer from '../components/footer/Footer'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import { useToasts } from 'react-toast-notifications'
 
 const ContactUs = () => {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [mobileNo, setMobileNo] = useState('')
+  const [query, setQuery] = useState('')
+  const router = useRouter()
+  const { addToast } = useToasts()
+
+
   useEffect(() => {
     AOS.init({
       duration: 2000,
     })
   }, [])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (name == '') {
+      addToast('Please enter the name!', { appearance: 'error' })
+
+      return false
+    }
+
+    if (email == '') {
+      addToast('Please enter the email!', { appearance: 'error' })
+      return false
+    }
+
+    if (IsEmail(email) == false) {
+      addToast('Incorrect email!', { appearance: 'error' })
+
+      return false
+    }
+
+    if (mobileNo == '') {
+      addToast('Please enter the mobile number!', { appearance: 'error' })
+      return false
+    }
+
+    if (mobileNo.length != 10) {
+      addToast('Mobile number must be of ten digits!', { appearance: 'error' })
+      return false
+    }
+
+    if (query == '') {
+      addToast('Please enter the query!', { appearance: 'error' })
+      return false
+    }
+
+
+    try {
+      const data = await axios.post(`https://phplaravel-709751-2547471.cloudwaysapps.com/api/contact-us`,{
+          "name":name,
+          "email":email,
+          "mobile_no":mobileNo,
+          "query":query,
+      });
+  
+      if(data.status == 200){
+        addToast("Success!", { appearance: 'success' });
+        router.push("/thanks");
+      }
+
+      //
+    } catch (err) {
+      console.log(err)
+      addToast('Invalid! Please try again.', { appearance: 'error' })
+    }
+  }
+
+  const IsEmail = (email) => {
+    let regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
+    if (!regex.test(email)) {
+      return false
+    } else {
+      return true
+    }
+  }
 
   return (
     <>
@@ -69,31 +146,48 @@ const ContactUs = () => {
         <div className="container">
           <div className="row">
             <div className="col-md-6 col-lg-6">
-              <form>
+            <form onSubmit={handleSubmit}>
                 <div className="customfrms lssmg">
                   <div className="form-group">
                     <input
                       type="textr"
                       className="form-control"
                       placeholder="First Name"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <input
-                      type="email"
-                      className="form-control"
-                      placeholder="Email Id"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
                     />
                   </div>
                   <div className="form-group">
                     <input
                       type="text"
                       className="form-control"
-                      placeholder="Phone Number"
+                      placeholder="Email Id"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
                     />
                   </div>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Mobile Number"
+                      value={mobileNo}
+                      onChange={(e) =>setMobileNo(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="form-group">
+                                    <textarea
+                                      type="text"
+                                      className="form-control"
+                                      placeholder="Query"
+                                      value={query}
+                                      onChange={(e) => setQuery(e.target.value)}
+                                    ></textarea>
+                                  </div>
+
                   <div className="form-group text-center">
-                    <button>Submit</button>
+                    <button type='submit'>Submit</button>
                   </div>
                 </div>
               </form>
